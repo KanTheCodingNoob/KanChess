b7b1ae21b629f925e5d0afe16d432b27710fb798
KanTheCodingNoob <s4019548@rmit.edu.vn>
2025-12-25 18:25:23 +0700

    Disable strict mode to prevent websocket from unmount after mount

--- kan_chess_frontend/src/main.tsx
+++ kan_chess_frontend/src/main.tsx
@@ -4,13 +4,12 @@ import './index.css'
 import {BrowserRouter} from "react-router";
 import Router from "./Router.tsx";
 import {SocketProvider} from "./features/socket/SocketContext.tsx";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+// const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <SocketProvider>
-          <BrowserRouter>
-              <Router />
-          </BrowserRouter>
-      </SocketProvider>
-  </StrictMode>,
+    <BrowserRouter>
+        <Router />
+    </BrowserRouter>
 )
