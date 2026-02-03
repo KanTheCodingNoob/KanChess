e5630c3802c49877547c7f7567269e898d617980
KanTheCodingNoob <s4019548@rmit.edu.vn>
2026-02-03 17:58:35 +0700

    Change configuration

--- kan_chess_backend/src/main/java/com/kan/kanchess/game/websocket/WebSocketConfig.java
+++ kan_chess_backend/src/main/java/com/kan/kanchess/game/websocket/WebSocketConfig.java
@@ -17,7 +17,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(handler, "")
-				.setAllowedOriginPatterns("*"); // Change before deployment
+		registry.addHandler(handler, "/ws")
+				.setAllowedOriginPatterns("*");
 	}
 }
--- kan_chess_frontend/src/config/backend.ts
+++ kan_chess_frontend/src/config/backend.ts
@@ -2,4 +2,4 @@ const HOST = import.meta.env.VITE_BACKEND_HOST;
 const PORT = import.meta.env.VITE_BACKEND_PORT;
 
 export const BACKEND_HTTP = `http://${HOST}:${PORT}`;
-export const BACKEND_WS = `ws://${HOST}:${PORT}`;
+export const BACKEND_WS = `ws://${HOST}:${PORT}/ws`;
